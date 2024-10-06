8d664ae2fb312334308ce0c57d931820fde3100e
Amir Czwink <amir130@hotmail.de>
2024-10-06 21:07:22 +0200

    added missing color for bootstrap

--- src/Bootstrap.ts
+++ src/Bootstrap.ts
@@ -16,4 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
 
-export type BootstrapColor = "primary" | "secondary" | "success";
+export type BootstrapColor = "danger" | "primary" | "secondary" | "success";
